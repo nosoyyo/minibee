9d986d7780b0067e7518116710824ef906fce2ff
Paul Carino <paulcarinocrafts@gmail.com>
2018-12-26 11:51:33 +0800

    add default user-agent when not specified

--- bees/abstractbee.py
+++ bees/abstractbee.py
@@ -59,6 +59,10 @@ class AbstractBee():
         '''
         resp = None
         headers = headers or self.headers.copy()
+        ua = 'User-Agent'
+        if not headers[ua] or not headers[ua.lower()]:
+            headers[ua] = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_2) \
+AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.0.3578.98 Safari/537.36'
         _params = _params or {}
 
         try:
